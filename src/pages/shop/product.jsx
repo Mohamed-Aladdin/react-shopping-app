552be91158ea88bd3943f7ae0f27c5e368f06d41
Mohamed Aladdin <moham3daladdin101@gmail.com>
2023-08-30 10:00:08 +0300

    Initital Commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import { ShopContext } from '../../context/shop-context'
+
+const Product = ({ data }) => {
+  const { id, productName, price, productImage } = data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemAmount = cartItems[id];
+
+  return (
+    <div className="product">
+      <img src={productImage} alt="productImage" />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p>${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>Add To Cart {cartItemAmount > 0 && <>({cartItemAmount})</>}</button>
+    </div>
+  );
+}
+
+export default Product;
